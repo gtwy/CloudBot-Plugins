af1de2ddd28eea8d4189dd6cf7a039f8b19b8733
James Watt <james@techdirect.io>
2019-09-15 18:29:47 -0400

    Closes #8

--- next_launch.py
+++ next_launch.py
@@ -25,31 +25,37 @@ async def ll_api():
 @hook.command('nextlaunch', 'nl')
 async def nextlaunch(text, message):
    lchsearch = text.strip()
+   # Download list of search results.
    try:
-      # Pull a list of search results. (If no search term was entered, pull the next launch.)
-      if not lchsearch:
+      if not lchsearch: # No search term entered, just pull the next launch
          lchlist = ll.Launch.fetch(llapi, next=1)
-      else:
+      else: # Search term entered. Download list of next 300 launches and then filter with search term
          lchlist = list(filter(lambda x: lchsearch.lower() in x.name.lower(), ll.Launch.fetch(llapi, next=300)))
    except Exception:
       message("Something went wrong, either you entered an invalid search string or the API is down.")
       raise
 
-   # First result
-   lch = lchlist[0]
+   # Are there any results?
+   if len(lchlist) > 0:
+      lch = lchlist[0]
 
-   # Name - Location. E.g.    'H-IIB 304 | Kounotori 8 (HTV-8) - Osaki Y LP2, Tanegashima, Japan - '
-   lchout = lch.name + ' - ' + lch.location.pads[0].name + ' - '
+      # Name - Location. E.g.    'H-IIB 304 | Kounotori 8 (HTV-8) - Osaki Y LP2, Tanegashima, Japan - '
+      lchout = lch.name + ' - ' + lch.location.pads[0].name + ' - '
 
-   # If a video feed exists, append to output
-   if len(lch.vid_urls) > 0:
-      lchout  += re.sub(r'(www\.){0,1}youtube\.com\/watch\?v=', 'youtu.be/', lch.vid_urls[0], flags=re.IGNORECASE) + ' - '
+      # If a video feed exists, append to output
+      if len(lch.vid_urls) > 0:
+         lchout  += re.sub(r'(www\.){0,1}youtube\.com\/watch\?v=', 'youtu.be/', lch.vid_urls[0], flags=re.IGNORECASE) + ' - '
 
-   # TBD or just NET. NET = No Earlier Than. If not TBD, put the countdown.
-   if lch.tbddate==1 or lch.tbdtime==1:
-      lchout += 'TBD/NET ' + str(lch.net)
+      # TBD or just NET. NET = No Earlier Than. If not TBD, put the countdown.
+      if lch.tbddate==1 or lch.tbdtime==1:
+         lchout += 'TBD/NET ' + str(lch.net)
+      else:
+         lchout += 'NET ' + str(lch.net) + ' - T-' + str(lch.net - datetime.now(timezone.utc))
    else:
-      lchout += 'NET ' + str(lch.net) + ' - T-' + str(lch.net - datetime.now(timezone.utc))
+      if not lchsearch:
+         lchout = 'Something went wrong. Maybe the Launch Library API is down?'
+      else:
+         lchout = 'No future launches found matching search term: ' + lchsearch
 
    # Output
    message(lchout)
